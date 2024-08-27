4838c0172e195967024ffad6a9d955c7c9111440
Gilbert Tan <gilbert.tan1991@gmail.com>
2024-08-27 23:51:17 +0800

    Created the header and its styling

--- includes/header.php
+++ includes/header.php
@@ -4,13 +4,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Document</title>
+    <link rel="icon" href="./image/fundamentum.ico" type="image/icon type">
+    <title>Fundamentum</title>
 </head>
 <body>
 
-<header>
-    <div>
-       <a href="index.php"><img src="https://www.royalcanin.com/~/media/Royal-Canin/Home/Logo.ashx" alt="logo"></a>
-    </div>
+<header> 
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+            <div class="d-flex justify-content-between align-items-center w-100">
+                <!-- Company Logo -->
+                <div class="company-logo">
+                    <a href="index.php" class="navbar-brand d-flex flex-row align-items-center text-decoration-none">
+                        <img src="./image/fundamentum.png" alt="fundamentum logo">
+                        <h1 class="text-white">Fundamentum</h1>
+                    </a>
+                </div>
+
+                <!-- Navbar Toggle Button -->
+                <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Income</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Expense</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+
+
+
 
 </heaader>
