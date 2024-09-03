57cc57eaf10a216f4c7fe9138256cf7496bc273f
Gilbert Tan <gilbert.tan1991@gmail.com>
2024-09-03 22:43:01 +0800

    Integrate the design

--- includes/header.php
+++ includes/header.php
@@ -41,7 +41,7 @@
                         <a class="nav-link" href="#">Expense</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Logout</a>
+                        <a class="nav-link" href="./logout.php">Logout</a>
                     </li>
                 </ul>
             </div>
