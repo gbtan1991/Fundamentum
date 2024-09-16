b138f7acf30f59c7b3b1df219590373e160f39ba
Gilbert Tan <gilbert.tan1991@gmail.com>
2024-09-16 23:11:11 +0800

    Add the fontawesome

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
-<div>
-    test
+<div class="">
+    
 </div>
-
+<script src="https://kit.fontawesome.com/266a593bd6.js" crossorigin="anonymous"></script>
 <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
 <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
