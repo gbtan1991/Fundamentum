3cb13ee2569ed92b661299e90c4b4ec2942848df
gbtan23 <gilbert.tan1991@gmail.com>
2026-04-09 20:07:27 +0800

    Remodeling the app

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="">
-    
-</div>
-<script src="https://kit.fontawesome.com/266a593bd6.js" crossorigin="anonymous"></script>
-<script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
-<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-</body>
-
-</html>
